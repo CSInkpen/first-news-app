15eac877a3e7552f1d08ff423934fbca7a4a7f33
CSInkpen <CSInkpen@gmail.com>
2017-07-26 10:56:05 -0600

    Created basic table

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+import csv
+
+
+
+# Instantiate Flask class instance as "app"
 app = Flask(__name__)  # Note the double underscores on each side!
 
+def get_csv():
+	csv_path = './static/la-riots-deaths.csv'
+	csv_file = open(csv_path, 'rt')
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
 
 
 @app.route("/")
 def index():
 	template = 'index.html'
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list=object_list)
 
 
 
